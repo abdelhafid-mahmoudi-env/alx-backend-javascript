89b15c247d7ffbae7e93470dfbdb0d4285954252
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-06-27 14:47:28 +0000

    test

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es6: true,
     jest: true,
   },
-  extends: 'eslint:recommended',
+  extends: ["airbnb-base", "plugin:jest/all"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
+  plugins: ["jest"],
   rules: {
-    'no-console': 'off',
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
   },
+  overrides: [
+    {
+      files: ["*.js"],
+      excludedFiles: "babel.config.js",
+    },
+  ],
 };
