eecb55e7a9dbacab0924ba885379d2928020d940
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-08-21 10:24:25 +0000

    test

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const server = express();
 
-server.get('/', (req, res) => {
+const app = express();
+
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-server.listen(1245, () => {
+app.listen(1245, () => {
   console.log('Server is running on port 1245');
 });
 
-module.exports = server;
+module.exports = app;
