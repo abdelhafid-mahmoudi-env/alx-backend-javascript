4f5ba602b10c3a7f3c60b1f3a74cafdc7b5a9361
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-08-21 10:56:43 +0000

    test

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -3,9 +3,9 @@
  * @author Abdelhafid Mahmoudi <https://github.com/abdelhafid-mahmoudi-env>
  */
 class AppController {
-	static getHomepage(request, response) {
-		response.status(200).send('Hello Holberton School!');
-	}
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
 }
 
 export default AppController;
